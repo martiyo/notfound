7bbeb7863f4aecc17491779cce029483c1812e35
martiyo <martiyo@protonmail.com>
2020-09-14 21:27:37 -0300

    Updates

---
+++ precache-manifest.95f90dbaea91a6ffa5d94f20129fe742.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbf066de80900a8d90bc5400c6029dc6",
+    "url": "/notfound/index.html"
+  },
+  {
+    "revision": "9dc409a1378ad6bdd4eb",
+    "url": "/notfound/static/css/main.19471cd4.chunk.css"
+  },
+  {
+    "revision": "5c2f1f2f739c3f8dfa2f",
+    "url": "/notfound/static/js/2.ca04a109.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/notfound/static/js/2.ca04a109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc409a1378ad6bdd4eb",
+    "url": "/notfound/static/js/main.321227ec.chunk.js"
+  },
+  {
+    "revision": "070e6617c00132d24cfc",
+    "url": "/notfound/static/js/runtime-main.0aa602b6.js"
+  }
+]);
